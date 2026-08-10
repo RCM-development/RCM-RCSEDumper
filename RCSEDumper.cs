845ab7ea7188513b778105f35826bcbeeb2b8622
connor-jt <ct770a@gmail.com>
2026-08-10 20:37:28 +0930

    added dumper action to help find entities with valid turret like aiming setups

--- RCSEDumper.cs
+++ RCSEDumper.cs
@@ -10,6 +10,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using TestMod;
 using UnityEngine;
+using UnityEngine.UIElements;
 namespace RCM_RCSEDumper{
 
     [BepInDependency(RCMManager.IDENTIFIER, BepInDependency.DependencyFlags.HardDependency)]
@@ -29,8 +30,9 @@ namespace RCM_RCSEDumper{
                 mod.CreateButtonField("Unlock Enemy Units", UnlockEnemyUnits);
                 mod.CreateButtonField("Export Units Json", ExportUnitsJson);
                 mod.CreateButtonField("Export Hacks Json", ExportHacksJson);
-                mod.CreateButtonField("Broken Upgrades Json", ExportUgradesJson);
+                //mod.CreateButtonField("Broken Upgrades Json", ExportUgradesJson);
                 mod.CreateButtonField("Export ID lists", ExportIDLists);
+                mod.CreateButtonField("Dump Aiming values", ExportAimingValues);
             }, TaskScheduler.FromCurrentSynchronizationContext());
         }
 
@@ -223,8 +225,208 @@ namespace RCM_RCSEDumper{
                 foreach (BlueprintPool blueprintPool in Resources.LoadAll<BlueprintPool>("BlueprintPools"))
                     writer.Write(blueprintPool.poolId + "\t");
             }
+
         
             RCMManager.Log("RCSE Dumper: successfully dumped ID's");
+        }
+
+        string ParseAiming(SingleTargetAction action)
+        {
+            string type = "No Aiming";
+            if (action == null) return type;
+
+            type = action.GetType().ToString();
+            
+            if (action.GetType() == typeof(RotateInSingleTargetDirectionAroundAxisAction))
+                type = type + (((RotateInSingleTargetDirectionAroundAxisAction)action).direction == RectTransform.Axis.Vertical ? " | Vertical" : " | Horizontal");
+            else if (action.GetType() == typeof(SerialSingleTargetAction))
+            {
+                SerialSingleTargetAction serialAction = (SerialSingleTargetAction)action;
+                for (int i = 0; i < serialAction.actions.Count; i++)
+                    type = type + " ["+i+"]: " + ParseAiming(serialAction.actions[i]);
+            }
+            return type;
+        }
+
+
+        void ExportAimingValues(){
+            if (!VerifyValid()) return;
+
+            List<string> no_prefab = new List<string>();
+            List<string> no_controller = new List<string>();
+            List<string> has_skill_aiming = new List<string>();
+            List<string> no_aiming = new List<string>();
+            List<string> has_child_entities = new List<string>();
+            List<string> laser_turret = new List<string>();
+            List<string> invalid_horizontal_pivot = new List<string>();
+            List<string> compatible = new List<string>();
+
+            List<string> exta_logs = new List<string>();
+
+
+            foreach (var item in EntityBalancingStore.EntityBalancingParametersList) {
+
+                string text = EntityBalancingStore.PrefabLocation(item.entityId);
+                var auto = Resources.Load(text);
+                if (auto == null) no_prefab.Add(item.entityId);
+                else {
+                    GameObject gameObject = (GameObject)GameObject.Instantiate(auto, new Vector3(0, 0, 0), Quaternion.identity);
+                    EntityController entityController = gameObject.GetComponent<EntityController>();
+
+                    if (entityController == null) no_controller.Add(item.entityId);
+                    else if (entityController.skillAiming != null) has_skill_aiming.Add(item.entityId);
+                    else if (entityController.aiming == null) no_aiming.Add(item.entityId);
+                    else if (entityController.childEntityControllers.Count > 0) has_child_entities.Add(item.entityId);
+                    else
+                    {
+
+                        // if using laser based weapon, drop it
+                        bool has_projectile = false;
+                        foreach (var e in entityController.events)
+                        {
+                            if (e.@event == EntityController.Event.OnReadyToShoot)
+                            {
+                                foreach (var a in e.actions)
+                                {
+                                    if (a.GetType() == typeof(ShootProjectile))
+                                    {
+                                        has_projectile = true;
+                                    }
+                                }
+                                foreach (var a in e.conditionalActions)
+                                {
+                                    foreach(var b in a.actions)
+                                    {
+                                        if (b.GetType() == typeof(ShootProjectile))
+                                        {
+                                            has_projectile = true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if (!has_projectile){
+                            laser_turret.Add(item.entityId);
+                            goto break_thingo;
+                        }
+
+                        // if using serial aiming, then we just check for more than 2 enties and its a definite yes
+                        SingleTargetAction aiming = entityController.aiming;
+                        if (entityController.aiming.GetType() == typeof(SerialSingleTargetAction))
+                        {
+                            SerialSingleTargetAction serialAction = (SerialSingleTargetAction)entityController.aiming;
+                            if (serialAction.actions.Count > 1){
+                                compatible.Add(item.entityId);
+                                goto break_thingo;
+                            } else if (serialAction.actions.Count == 1){
+                                aiming = serialAction.actions[0];
+                            } else if (serialAction.actions.Count == 0){
+                                no_aiming.Add(item.entityId);
+                                exta_logs.Add(item.entityId + " has serial but no enties, marked as no aiming.");
+                                goto break_thingo;
+                            }
+                        }
+
+                        // now we evaluate the current aiming to see if it holds an actual turret
+                        string aiming_root_name = "";
+                        GameObject aiming_root_parent = null;
+                        if (aiming.GetType() == typeof(RotateInSingleTargetDirectionAroundAxisAction)){
+                            RotateInSingleTargetDirectionAroundAxisAction curr = (RotateInSingleTargetDirectionAroundAxisAction)aiming;
+                            aiming_root_name = curr.transformToRotate.gameObject.name;
+                            aiming_root_parent = curr.transformToRotate.parent.gameObject;
+                        }
+                        else if (aiming.GetType() == typeof(RotateInSingleTargetDirectionAction)){
+                            RotateInSingleTargetDirectionAction curr = (RotateInSingleTargetDirectionAction)aiming;
+                            aiming_root_name = curr.transformToRotate.gameObject.name;
+                            aiming_root_parent = curr.transformToRotate.parent.gameObject;
+                        }
+                        else if (aiming.GetType() == typeof(RotateToBallisticAngleSingleTargetAction)){
+                            RotateToBallisticAngleSingleTargetAction curr = (RotateToBallisticAngleSingleTargetAction)aiming;
+                            aiming_root_name = curr.transformToRotate.gameObject.name;
+                            aiming_root_parent = curr.transformToRotate.parent.gameObject;
+                        }
+                        else{
+                            no_aiming.Add(item.entityId);
+                            exta_logs.Add(item.entityId + " has unsupported aiming type, marking no aiming");
+                            goto break_thingo;
+                        }
+
+                        aiming_root_name = aiming_root_name.ToLower();
+
+                        switch (aiming_root_name){
+                            case "rothor":
+                            case "gun":
+                            case "simplemachinegunmesh": // armored oil harvester
+                            case "castleroofmachinegun":
+                            case "head": // animals mostly
+                            case "turretdoublecannon":
+                            case "hexagon_castlebattlement":
+                            case "rot hor": // spider tank
+                            case "tankhead":
+                            case "vehiclesimplemachinegun":
+                            case "rotor":
+                            case "rotator":
+                            case "rotatehorizontal":
+                            case "aimer":
+                            case "turrethead": // PCX launcher factory
+                            case "horizontalrotator": // PCX mobile command
+                            case "upperbody":
+                            case "detail_pipeend 1 (1)": // PCX termite
+                            case "horizontalhealarm": // repair trike
+                            case "detail_pipeend": 
+                            case "detailtargetsight (1)": // PCX big bomer
+
+                                compatible.Add(item.entityId);
+                                if (gameObject == aiming_root_parent)
+                                    exta_logs.Add(item.entityId + " has whitelisted name (" + aiming_root_name + ") but is top level child, so it must be incorrectly identified");
+                                 break;
+                            default:
+                                invalid_horizontal_pivot.Add(item.entityId);
+                                if (gameObject == aiming_root_parent)
+                                    exta_logs.Add(item.entityId + " has bad horizontal pivot, obj name: " + aiming_root_name + " also is a top level child...");
+                                else exta_logs.Add(item.entityId + " has bad horizontal pivot, obj name: " + aiming_root_name);
+                                break;
+                        }
+                        
+                    }
+                break_thingo:
+                    GameObject.Destroy(gameObject);
+                }
+
+
+            }
+            using (StreamWriter writer = new StreamWriter(export_folder + "aiming_values.txt"))
+            {
+                foreach (string s in no_prefab)
+                    writer.WriteLine(s + ": no prefab");
+                writer.WriteLine("====================================================================");
+                foreach (string s in no_controller)
+                    writer.WriteLine(s + ": no controller");
+                writer.WriteLine("====================================================================");
+                foreach (string s in has_skill_aiming)
+                    writer.WriteLine(s + ": has skill aiming");
+                writer.WriteLine("====================================================================");
+                foreach (string s in no_aiming)
+                    writer.WriteLine(s + ": no aiming");
+                writer.WriteLine("====================================================================");
+                foreach (string s in has_child_entities)
+                    writer.WriteLine(s + ": contains child entities");
+                writer.WriteLine("====================================================================");
+                foreach (string s in laser_turret)
+                    writer.WriteLine(s + ": likely uses a laser attack");
+                writer.WriteLine("====================================================================");
+                foreach (string s in invalid_horizontal_pivot)
+                    writer.WriteLine(s + ": no horizontal pivot");
+                writer.WriteLine("====================================================================");
+                foreach (string s in compatible)
+                    writer.WriteLine(s + ": compatible");
+                writer.WriteLine("====================================================================");
+                writer.WriteLine("============================= ERRORS ===============================");
+                writer.WriteLine("====================================================================");
+                foreach (string s in exta_logs)
+                    writer.WriteLine(s);
+            }
+            RCMManager.Log("RCSE Dumper: successfully dumped aiming values");
         }
     }
 }
